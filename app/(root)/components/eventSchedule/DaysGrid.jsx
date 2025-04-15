99460824b469eef0e545a93893c7d123806c3751
jakia911 <gazijakia911@gmail.com>
2025-04-15 21:56:15 +0600

    add

--- app/(root)/components/eventSchedule/DaysGrid.jsx
+++ app/(root)/components/eventSchedule/DaysGrid.jsx
@@ -10,11 +10,11 @@ const DaysGrid = () => {
   ];
 
   return (
-    <div className="w-full  grid grid-cols-7 gap-3 p-4">
+    <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-3 p-4">
       {days.map((day, index) => (
         <button
           key={index}
-          className="bg-transparent border border-white/30 text-white rounded-lg py-2 px-[50px] text-center hover:bg-white/10 transition"
+          className="bg-transparent border border-white/30 text-white rounded-lg py-2 px-4 text-center hover:bg-white/10 transition"
         >
           {day}
         </button>
